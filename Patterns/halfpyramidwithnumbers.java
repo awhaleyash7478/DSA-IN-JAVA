80b0e9f8007c7735520505111f8a948b9af04cda
awhaleyash7478 <awhaleyash7478@gmail.com>
2026-07-24 16:38:19 +0530

    inverted half pyramid with numbers

--- Patterns/halfpyramidwithnumbers.java
+++ Patterns/halfpyramidwithnumbers.java
@@ -1,11 +1,11 @@
-public class invertedhalfpyramidwithnumbers
+public class halfpyramidwithnumbers
 {
     public static void main(String[] args) {
         for(int i=1;i<=4;i++)
         {
-            for(int j=1;j<=4-i+1;j++  )
+            for(int j=1;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
