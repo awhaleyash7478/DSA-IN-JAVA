e1d11c2bca37f1c4c40390ff351c657436341544
awhaleyash7478 <awhaleyash7478@gmail.com>
2026-07-25 11:01:51 +0530

    diamond pyramnid

---
+++ AdvancePatterns/DiamondPattern.java
@@ -0,0 +1,34 @@
+
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++)
+        {
+            int spaces=n-i;
+            for(int s=1;s<=spaces;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+           for(int i=n-1;i>=1;i--)
+        {
+            int spaces=n-i;
+            for(int s=1;s<=spaces;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
